f6d413c55f328a252c41666b8af913d37078da36
abrarmusa <abrar.musa.89@gmail.com>
2014-03-25 00:45:26 -0700

    made navbar black

--- layout.php
+++ layout.php
@@ -1,9 +1,10 @@
 
 <!-- Import all the files we need -->
 <head>
-<link href="css/site.css" rel="stylesheet" media="screen">
+
 <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 <link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+<link href="css/site.css" rel="stylesheet" media="screen">
 <script type='text/javascript' src='//code.jquery.com/jquery-2.1.0.js'></script>
 <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
@@ -12,15 +13,14 @@
 
 
 <!-- Navbar -->
-<nav class="navbar navbar-default navbar-fixed-top navbar-inner " role="navigation">
+<nav class="navbar navbar-inverse navbar-fixed-top navbar-inner-fly" role="navigation">
   <div class="container">
-    <div class="navbar-header">
+<!--     <div class="navbar-header"> -->
       <ul class="nav navbar-nav">
-
         <li><a href="./index.php">Patternizer</a></li>
         <li><a href="./about.php">About</a></li>
       </ul>
-    </div>
+<!--     </div> -->
   </div><!-- /.container-fluid -->
 </nav>
 
